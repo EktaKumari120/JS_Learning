77fa886f68ee1a9d21fb74547b335d7bb3117c56
EktaKumari120 <ektakumari9784>
2025-10-21 16:53:42 +0530

    Organize files in another folder and renamed them

--- 01_basics/05_Day_conversionOperation.js
+++ 01_basics/05_Day_conversionOperation.js
@@ -1,6 +1,6 @@
 let score = "robot"
 
-//console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
@@ -30,30 +30,30 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2/2);
-console.log(2**2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
 
 let str1 = "hello"
 let str2 = "hitesh"
 
 let str3 = str1+str2
-console.log(str3);
+// console.log(str3);
 
-console.log("1"+2);
-console.log(1 + "2");
-console.log("1"+2+2);
-console.log("1" + 2 + "2");
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log("1" + 2 + "2");
 
-console.log((3+4)*5%3);
+// console.log((3+4)*5%3);
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
 
 let num1, num2, num3
 num1 = num2 = num3 = 2+2
