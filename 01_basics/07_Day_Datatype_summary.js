76f5ba6720a27340bef8f513b330d08305a3941c
EktaKumari120 <ektakumari9784>
2025-10-25 18:53:36 +0530

    added datatype summary

---
+++ 01_basics/07_Day_Datatype_summary.js
@@ -0,0 +1,34 @@
+//Primitive Datatypes
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 48797852n
+
+
+// Reference Type (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name : "robot",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp)
+console.log(typeof bigNumber)
+console.log(typeof myFunction)
